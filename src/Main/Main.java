cb4eda4620f2c6c83c994b2b55b45917dae65d0a
Marco Beraldi <marcobr92@hotmail.com>
2015-10-10 15:38:30 -0300

    Casos de teste de 1 a 20 implementados

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -15,76 +15,80 @@ public class Main
 	{
 		int opt = 0;
 		
-		System.out.println("Informe o teste");
+		System.out.println("Informe o teste a ser realizado ou digite 0 para sair");
 		Scanner ler = new Scanner(System.in);
 		
-
-		opt = ler.nextInt();
-        switch (opt) {
-        case 1:	
-        	test1();
-        	break;
-        case 2:	
-        	test2();
-        	break;
-        case 3:	
-        	test3();
-        	break;
-        case 4:	
-        	test4();
-        	break;
-        case 5:	
-        	test5();
-        	break;
-        case 6:	
-        	test6();
-        	break;
-        case 7:	
-        	test7();
-        	break;
-        case 8:	
-        	test8();
-        	break;
-        case 9:	
-        	test9();
-        	break;
-        case 10:	
-        	test10();
-        	break;
-        case 11:	
-        	test11();
-        	break;
-        case 12:	
-        	test12();
-        	break;
-        case 13:	
-        	test13();
-        	break;
-        case 14:	
-        	test14();
-        	break;
-        case 15:	
-        	test15();
-        	break;
-        case 16:	
-        	test16();
-        	break;
-        case 17:	
-        	test17();
-        	break;
-        case 18:	
-        	test18();
-        	break;
-        case 19:	
-        	test19();
-        	break;
-        case 20:	
-        	test20();
-        	break;
+		do{
+			opt = ler.nextInt();
+			switch (opt) {
+			case 0:	
+				System.out.println("Finalizando a interface de teste");
+				break;
+			case 1:	
+				test1();
+				break;
+			case 2:	
+				test2();
+				break;
+			case 3:	
+				test3();
+				break;
+			case 4:	
+				test4();
+				break;
+			case 5:	
+				test5();
+				break;
+			case 6:	
+				test6();
+				break;
+			case 7:	
+				test7();
+				break;
+			case 8:	
+				test8();
+				break;
+			case 9:	
+				test9();
+				break;
+			case 10:	
+				test10();
+				break;
+			case 11:	
+				test11();
+				break;
+			case 12:	
+				test12();
+				break;
+			case 13:	
+				test13();
+				break;
+			case 14:	
+				test14();
+				break;
+			case 15:	
+				test15();
+				break;
+			case 16:	
+				test16();
+				break;
+			case 17:	
+				test17();
+				break;
+			case 18:	
+				test18();
+				break;
+			case 19:	
+				test19();
+				break;
+			case 20:	
+				test20();
+				break;
             default:
                 System.out.println("Este não é um teste válido!");
-        }
-		
+			}
+		}while(opt!=0);
+		ler.close();
 		
 		/*
 		 * TODO verificar este cenario. pois se executado N vezes em algumas situacoes
@@ -92,6 +96,7 @@ public class Main
 		 */
 	}
 	
+	//teste original do grupo de desenvolvimento
 	public static void test0()
 	{
 		Escalonador escalonador = new Escalonador();
@@ -186,45 +191,45 @@ public class Main
 		
 		List<Aluno> alunos= new ArrayList<>();
 		alunos.add(new Aluno(new Agenda(new boolean[]
-				{true, true,true, true,true, true,true, true,true, true})));
+				{false, false, false, false, false, false, false, false, false, false})));
 		alunos.add(new Aluno(new Agenda(new boolean[]
-				{true, true,true, true,true, true,true, true,true, true})));
+				{false, false, false, false, false, false, false, false, false, false})));
 		alunos.add(new Aluno(new Agenda(new boolean[]
-				{true, true,true, true,true, true,true, true,true, true})));
+				{false, false, false, false, false, false, false, false, false, false})));
 		alunos.add(new Aluno(new Agenda(new boolean[]
-				{true, true,true, true,true, true,true, true,true, true})));
+				{false, false, false, false, false, false, false, false, false, false})));
 		alunos.add(new Aluno(new Agenda(new boolean[]
-				{true, true,true, true,true, true,true, true,true, true})));
+				{false, false, false, false, false, false, false, false, false, false})));
 		alunos.add(new Aluno(new Agenda(new boolean[]
-				{true, true,true, true,true, true,true, true,true, true})));
+				{false, false, false, false, false, false, false, false, false, false})));
 		alunos.add(new Aluno(new Agenda(new boolean[]
-				{true, true,true, true,true, true,true, true,true, true})));
+				{false, false, false, false, false, false, false, false, false, false})));
 		alunos.add(new Aluno(new Agenda(new boolean[]
-				{true, true,true, true,true, true,true, true,true, true})));
+				{false, false, false, false, false, false, false, false, false, false})));
 		alunos.add(new Aluno(new Agenda(new boolean[]
-				{true, true,true, true,true, true,true, true,true, true})));
+				{false, false, false, false, false, false, false, false, false, false})));
 		alunos.add(new Aluno(new Agenda(new boolean[]
-				{true, true,true, true,true, true,true, true,true, true})));
+				{false, false, false, false, false, false, false, false, false, false})));
 		alunos.add(new Aluno(new Agenda(new boolean[]
-				{true, true,true, true,true, true,true, true,true, true})));
+				{false, false, false, false, false, false, false, false, false, false})));
 		alunos.add(new Aluno(new Agenda(new boolean[]
-				{true, true,true, true,true, true,true, true,true, true})));
+				{false, false, false, false, false, false, false, false, false, false})));
 		alunos.add(new Aluno(new Agenda(new boolean[]
-				{true, true,true, true,true, true,true, true,true, true})));
+				{false, false, false, false, false, false, false, false, false, false})));
 		alunos.add(new Aluno(new Agenda(new boolean[]
-				{true, true,true, true,true, true,true, true,true, true})));
+				{false, false, false, false, false, false, false, false, false, false})));
 		alunos.add(new Aluno(new Agenda(new boolean[]
-				{true, true,true, true,true, true,true, true,true, true})));
+				{false, false, false, false, false, false, false, false, false, false})));
 		alunos.add(new Aluno(new Agenda(new boolean[]
-				{true, true,true, true,true, true,true, true,true, true})));
+				{false, false, false, false, false, false, false, false, false, false})));
 		alunos.add(new Aluno(new Agenda(new boolean[]
-				{true, true,true, true,true, true,true, true,true, true})));
+				{false, false, false, false, false, false, false, false, false, false})));
 		alunos.add(new Aluno(new Agenda(new boolean[]
-				{true, true,true, true,true, true,true, true,true, true})));
+				{false, false, false, false, false, false, false, false, false, false})));
 		alunos.add(new Aluno(new Agenda(new boolean[]
-				{true, true,true, true,true, true,true, true,true, true})));
+				{false, false, false, false, false, false, false, false, false, false})));
 		alunos.add(new Aluno(new Agenda(new boolean[]
-				{true, true,true, true,true, true,true, true,true, true})));
+				{false, false, false, false, false, false, false, false, false, false})));
 		
 		Agenda a = escalonador.escalonar(alunos, (byte)1);
 		
